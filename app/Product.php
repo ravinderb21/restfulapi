06958a63b850b0aa0dcf376135078e73d227ef01
Ravinder Bhatia <ravinder@ilscorp.com>
2018-02-14 09:16:14 -0800

    first commit

---
+++ app/Product.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+use App\Category;
+use App\Seller;
+use App\Transaction;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+	const AVAILABLE_PRODUCT = 'available';
+	const UNAVAILABLE_PRODUCT = 'unavailable';
+
+	protected $fillable = [
+  	'name',
+  	'description',
+  	'quatity',
+  	'status',
+  	'image',
+  	'seller_id'
+  ];
+
+	public function isAvailable()
+	{
+		return $this->status == Product::AVAILABLE_PRODUCT;
+	}
+
+	public function seller()
+	{
+		return $this->belongsTo(Seller::class);
+	}
+
+	public function transactions()
+	{
+		return $this->hasMany(Transaction::class);
+	}
+
+	public function categories()
+	{
+		return $this->belongsToMany(Category::class);
+	}
+
+}
